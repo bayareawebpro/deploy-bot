5387ecc2da045ecf364a92a6b45b5a2e04a943a8
Dan Alvidrez <dan@bayareawebpro.com>
2019-06-15 07:28:08 -0700

    Update trim.sh

--- resources/scripts/snapshots/trim.sh
+++ resources/scripts/snapshots/trim.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
-if [[ -z $2 ]]; then
-    SNAPSHOTS_MAX=8
-else
-    SNAPSHOTS_MAX=$2
-fi
-cd $1 && rm -f `ls -t | awk NR> ${SNAPSHOTS_MAX}`;
+cd $1 && rm -f `ls -t | awk NR> $2`;
