05837a6f71bbd479bc614a61021b376be53ebdd5
Dan Alvidrez <dan@bayareawebpro.com>
2019-06-15 07:22:59 -0700

    Update trim.sh

--- resources/scripts/snapshots/trim.sh
+++ resources/scripts/snapshots/trim.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
-if [[ -d $1 ]]; then
-    cd $1 && rm -f `ls -t | awk NR> $2`;
-    echo "Snapshots directory cleaned successfully.";
-else
-    echo "Snapshots directory does not exist." 1>&2;
-fi
+cd $1 && rm -f `ls -t | awk NR> $2`;
