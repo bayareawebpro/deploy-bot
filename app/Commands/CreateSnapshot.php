d151513b60b6c1e4420a6f97529fba47c73b7401
Dan Alvidrez <dan@bayareawebpro.com>
2019-06-15 07:06:28 -0700

    Update CreateSnapshot.php

--- app/Commands/CreateSnapshot.php
+++ app/Commands/CreateSnapshot.php
@@ -29,10 +29,13 @@ class CreateSnapshot extends Command
      */
     public function handle()
     {
+        //Insure Plenty of Resources.
         @ini_set('max_execution_time', 10080);
-        @ini_set('memory_limit', '100M');
+        @ini_set('memory_limit', '500M');
+
+        //Run the Command Sequence.
         $env = $this->argument('env');
-        if(method_exists($this, $env)){
+        if (method_exists($this, $env)) {
             $this->$env($this->argument('hash'));
         }
     }
@@ -43,42 +46,42 @@ class CreateSnapshot extends Command
      */
     protected function production($hash)
     {
+        //Alert Users.
         $alert = "Deploying Staging to Production";
         $this->alert($alert); SlackApi::message("*$alert*");
 
+        //Check for Existing Release Snapshot.
         $isNewRelease = (!Storage::disk('production')->exists("$hash.sql"));
-        $path = config('filesystems.disks.production.root');
+
+        //Snapshots Directory / Insure Exists.
+        $path = $this->makeDirectory(config('filesystems.disks.production.root'));
+
+        //Current Snapshot Path.
         $snapshot = "$path/$hash.sql";
 
-        //Insure Directory Exists.
-        $this->makeDirectory($path);
-
         //Create Production Snapshot from Staging Database.
-        if($isNewRelease){
-            $this->stopOnFailure(
-                $this->task("Creating Snapshot $hash from Staging", function() use ($snapshot){
-                    return Bash::script("local", 'snapshots/dump', "staging $snapshot")->isSuccessful();
-                })
-            );
-            SlackApi::message("Created Snapshot $hash from Staging Successfully. ($snapshot)");
+        if ($isNewRelease) {
+            if($this->isSuccessful(
+                Bash::script("local", 'snapshots/dump', "staging $snapshot")
+            )){
+                SlackApi::message("Created Snapshot $hash from Staging Successfully. ($snapshot)");
+            }
         }
 
         //Load Production Snapshot into Live Database.
-        $this->stopOnFailure(
-            $this->task("Loading Snapshot $hash to Production", function() use ($snapshot){
-                return Bash::script("local", 'snapshots/load', "production $snapshot")->isSuccessful();
-            })
-        );
-        SlackApi::message("Loaded Snapshot $hash to Production Successfully. ($snapshot)");
+        if($this->isSuccessful(
+            Bash::script("local", 'snapshots/load', "production $snapshot")
+        )){
+            SlackApi::message("Loaded Snapshot $hash to Production Successfully. ($snapshot)");
+        }
 
         //Cleaning Up Old Snapshots.
-        if($isNewRelease){
-            $this->task("Cleaning Up Old Snapshots", function() use ($path){
-                return (
-                    Bash::script("local", 'snapshots/trim', $path)->isSuccessful()
-                );
-            });
-            SlackApi::message("Old Snapshots Cleaned Up Successfully.");
+        if ($isNewRelease) {
+            if($this->isSuccessful(
+                Bash::script("local", 'snapshots/trim', $path)
+            )){
+                SlackApi::message("Old Snapshots Cleaned Up Successfully.");
+            }
         }
     }
 
@@ -88,55 +91,64 @@ class CreateSnapshot extends Command
      */
     protected function staging($hash)
     {
+        //Alert Users.
         $alert = "Creating Staging Database Snapshot";
         $this->alert($alert); SlackApi::message("*$alert*");
 
-        $path = config('filesystems.disks.staging.root');
+        //Snapshots Directory / Insure Exists.
+        $path = $this->makeDirectory(config('filesystems.disks.staging.root'));
+
+        //Current Snapshot Path.
         $snapshot = "$path/$hash.sql";
 
-        //Insure Directory Exists.
-        $this->makeDirectory($path);
-
         //Create Snapshot for Staging Database.
-        $this->stopOnFailure(
-            $this->task("Create Snapshot $hash for Staging", function() use ($snapshot){
-                return Bash::script("localhost", 'snapshots/dump', "staging $snapshot")->isSuccessful();
-            })
-        );
-        SlackApi::message("Staging Snapshot Created Successfully. ($snapshot)");
+        if($this->isSuccessful(
+            Bash::script("local", 'snapshots/dump', "staging $snapshot")
+        )){
+            SlackApi::message("Staging Snapshot Created Successfully. ($snapshot)");
+        }
 
         //Cleaning Up Old Snapshots.
-        $this->task("Cleaning Up Old Snapshots", function() use ($path){
-            return Bash::script("local", 'snapshots/trim', $path)->isSuccessful();
-        });
-
-        SlackApi::message("Old Snapshots Cleaned Up Successfully.");
+        if($this->isSuccessful(
+            Bash::script("local", 'snapshots/trim', $path)
+        )){
+            SlackApi::message("Old Snapshots Cleaned Up Successfully.");
+        }
     }
 
     /**
-     * Stop on Failure
-     * @param bool $status
+     * Log Bash Results
+     * @param Bash $result
+     * @return bool
      */
-    protected function stopOnFailure(bool $status){
-        if($status === false){
+    protected function isSuccessful(Bash $result)
+    {
+        $result->output()->each(function ($line) {
+            $this->{$line->type}($line->buffer);
+        });
+        if (!$result->isSuccessful()) {
             SlackApi::message("An error was encountered. Process Aborted.");
             exit;
         }
+        return $result->isSuccessful();
     }
 
     /**
      * Make New Directory
      * @param string $path
+     * @return string $path;
      */
-    protected function makeDirectory(string $path){
-        if(!File::isDirectory($path)){
+    protected function makeDirectory(string $path)
+    {
+        if (!File::isDirectory($path)) {
             File::makeDirectory($path, 0755, true);
         }
+        return $path;
     }
 
     /**
      * Define the command's schedule.
-     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     public function schedule(Schedule $schedule): void
